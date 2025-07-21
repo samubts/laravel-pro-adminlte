86a770fbb4b7fea650dfb8e4f27bfd9c16e92e04
Samuel <samuelbts1993l@gmail.com>
2025-07-21 16:06:25 -0300

    Estudos dia 21/07/2025

---
+++ resources/js/app.js
@@ -0,0 +1,8 @@
+import.meta.glob([
+    '../images/**',
+    '../fonts/**',
+]);
+
+import '/node_modules/bootstrap/dist/js/bootstrap.esm.js';
+import 'admin-lte';
+import './overlayscrollbars';
---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,60 @@
+@extends('layouts.auth')
+
+@section('body-class', 'register-page')
+
+@section('content')
+<div class="register-box">
+    <div class="register-logo">
+        <a href="../index2.html"><b>Admin</b>LTE</a>
+    </div>
+    <div class="card">
+        <div class="card-body register-card-body">
+            <p class="register-box-msg">Register a new membership</p>
+            <form action="{{ route('register') }}" method="post">
+                @csrf
+                <div class="input-group mb-3">
+                    <div class="input-group-text"><span class="bi bi-person"></span></div>
+                    <input type="text" name="name" class="form-control @error('name') is-invalid @enderror" placeholder="Full Name" autocomplete="name" value="{{ old('name') }}"/>
+                    @error('name')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                    @enderror
+                </div>
+
+                <div class="input-group mb-3">
+                    <div class="input-group-text"><span class="bi bi-envelope"></span></div>
+                    <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" placeholder="Email" autocomplete="email" value="{{ old('email') }}"/>
+                    @error('email')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                    @enderror
+                </div>
+
+                <div class="input-group mb-3">
+                    <div class="input-group-text"><span class="bi bi-lock-fill"></span></div>
+                    <input type="password" name="password" class="form-control @error('password') is-invalid @enderror" placeholder="Password" autocomplete="off"/>
+                    @error('password')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                    @enderror
+                </div>
+
+                <div class="input-group mb-3">
+                    <div class="input-group-text"><span class="bi bi-lock-fill"></span></div>
+                    <input type="password" name="password_confirmation" class="form-control" placeholder="Password Confirmation" autocomplete="off"/>
+                </div>
+
+                <div class="d-grid gap-2">
+                    <button type="submit" class="btn btn-primary">Register</button>
+                </div>
+            </form>
+            <p class="mb-0 text-center">
+                <a href="login.html" class="text-center"> I already have a membership </a>
+            </p>
+        </div>
+    </div>
+</div>
+@endsection
