8806f80dd7ce036712b81eececda1ec75a91f549
Samuel <samuelbts1993l@gmail.com>
2025-07-21 21:20:57 -0300

    Estudos 21/07/2025 NOITE

--- resources/views/auth/forgot_password.blade.php
+++ resources/views/auth/forgot_password.blade.php
@@ -1,1 +1,45 @@
-Esqueci a senha
+@extends('layouts.auth')
+
+@section('body-class', 'login-page')
+
+@section('content')
+    <div class="login-box">
+        <div class="login-logo">
+            <a href="{{ route('login') }}"><b>Admin</b>LTE</a>
+        </div>
+        <div class="card">
+            <div class="card-body login-card-body">
+                <p class="login-box-msg">Reset password</p>
+
+                @if (session('status'))
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
+                @endif
+
+
+                <form action="{{ route('password.email') }}" method="post">  
+                    @csrf
+                    <div class="input-group mb-3">
+                        <div class="input-group-text"><span class="bi bi-envelope"></span></div>
+                        <input type="email" name="email" autocomplete="email" class="form-control @error('email') is-invalid @enderror" placeholder="Email" value="{{ old('email') }}"/>
+                        @error('email')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+
+                    <div class="d-grid gap-2">
+                        <button type="submit" class="btn btn-primary">Send me the link</button>
+                    </div>
+                </form>
+                <div class="mt-2 text-center">
+                    <p class="mb-0">
+                        <a href="{{ route('login') }}" class="text-center">Back to login</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
--- resources/views/parts/header.blade.php
+++ resources/views/parts/header.blade.php
@@ -1,7 +1,5 @@
 <nav class="app-header navbar navbar-expand bg-body">
-  <!--begin::Container-->
   <div class="container-fluid">
-    <!--begin::Start Navbar Links-->
     <ul class="navbar-nav">
       <li class="nav-item">
         <a class="nav-link" data-lte-toggle="sidebar" href="#" role="button">
@@ -11,17 +9,12 @@
       <li class="nav-item d-none d-md-block"><a href="#" class="nav-link">Home</a></li>
       <li class="nav-item d-none d-md-block"><a href="#" class="nav-link">Contact</a></li>
     </ul>
-    <!--end::Start Navbar Links-->
-    <!--begin::End Navbar Links-->
     <ul class="navbar-nav ms-auto">
-      <!--begin::Navbar Search-->
       <li class="nav-item">
         <a class="nav-link" data-widget="navbar-search" href="#" role="button">
           <i class="bi bi-search"></i>
         </a>
       </li>
-      <!--end::Navbar Search-->
-      <!--begin::Messages Dropdown Menu-->
       <li class="nav-item dropdown">
         <a class="nav-link" data-bs-toggle="dropdown" href="#">
           <i class="bi bi-chat-text"></i>
@@ -29,7 +22,6 @@
         </a>
         <div class="dropdown-menu dropdown-menu-lg dropdown-menu-end">
           <a href="#" class="dropdown-item">
-            <!--begin::Message-->
             <div class="d-flex">
               <div class="flex-shrink-0">
                 <img
@@ -51,11 +43,9 @@
                 </p>
               </div>
             </div>
-            <!--end::Message-->
           </a>
           <div class="dropdown-divider"></div>
           <a href="#" class="dropdown-item">
-            <!--begin::Message-->
             <div class="d-flex">
               <div class="flex-shrink-0">
                 <img
@@ -77,11 +67,9 @@
                 </p>
               </div>
             </div>
-            <!--end::Message-->
           </a>
           <div class="dropdown-divider"></div>
           <a href="#" class="dropdown-item">
-            <!--begin::Message-->
             <div class="d-flex">
               <div class="flex-shrink-0">
                 <img
@@ -103,14 +91,11 @@
                 </p>
               </div>
             </div>
-            <!--end::Message-->
           </a>
           <div class="dropdown-divider"></div>
           <a href="#" class="dropdown-item dropdown-footer">See All Messages</a>
         </div>
       </li>
-      <!--end::Messages Dropdown Menu-->
-      <!--begin::Notifications Dropdown Menu-->
       <li class="nav-item dropdown">
         <a class="nav-link" data-bs-toggle="dropdown" href="#">
           <i class="bi bi-bell-fill"></i>
@@ -137,16 +122,12 @@
           <a href="#" class="dropdown-item dropdown-footer"> See All Notifications </a>
         </div>
       </li>
-      <!--end::Notifications Dropdown Menu-->
-      <!--begin::Fullscreen Toggle-->
       <li class="nav-item">
         <a class="nav-link" href="#" data-lte-toggle="fullscreen">
           <i data-lte-icon="maximize" class="bi bi-arrows-fullscreen"></i>
           <i data-lte-icon="minimize" class="bi bi-fullscreen-exit" style="display: none"></i>
         </a>
       </li>
-      <!--end::Fullscreen Toggle-->
-      <!--begin::User Menu Dropdown-->
       <li class="nav-item dropdown user-menu">
         <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
           <img
@@ -154,10 +135,9 @@
             class="user-image rounded-circle shadow"
             alt="User Image"
           />
-          <span class="d-none d-md-inline">Alexander Pierce</span>
+          <span class="d-none d-md-inline">{{ auth()->user()->name }}</span>
         </a>
         <ul class="dropdown-menu dropdown-menu-lg dropdown-menu-end">
-          <!--begin::User Image-->
           <li class="user-header text-bg-primary">
             <img
               src="{{ Vite::asset('resources/images/user2-160x160.jpg') }}"
@@ -165,33 +145,26 @@
               alt="User Image"
             />
             <p>
-              Alexander Pierce - Web Developer
+              {{ auth()->user()->name }} - Web Developer
               <small>Member since Nov. 2023</small>
             </p>
           </li>
-          <!--end::User Image-->
-          <!--begin::Menu Body-->
           <li class="user-body">
-            <!--begin::Row-->
             <div class="row">
               <div class="col-4 text-center"><a href="#">Followers</a></div>
               <div class="col-4 text-center"><a href="#">Sales</a></div>
               <div class="col-4 text-center"><a href="#">Friends</a></div>
             </div>
-            <!--end::Row-->
           </li>
-          <!--end::Menu Body-->
-          <!--begin::Menu Footer-->
           <li class="user-footer">
+            <form action="{{ route('logout') }}" method="POST">
+              @csrf
+              <button type="submit" class="btn btn-default btn-flat float-end">Logout</button>
+            </form>
             <a href="#" class="btn btn-default btn-flat">Profile</a>
-            <a href="#" class="btn btn-default btn-flat float-end">Sign out</a>
           </li>
-          <!--end::Menu Footer-->
         </ul>
       </li>
-      <!--end::User Menu Dropdown-->
     </ul>
-    <!--end::End Navbar Links-->
   </div>
-  <!--end::Container-->
 </nav>
